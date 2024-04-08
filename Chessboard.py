b5dd387227673781590cfb719ba55ee073af8a47
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-04-08 11:50:29 +0200

    Rook moves

--- Chessboard.py
+++ Chessboard.py
@@ -45,8 +45,37 @@ class Rook(ChessPiece):
         return "♖" if self.color == "white" else "♜"
 
     def get_possible_moves(self) -> List[Position]:
-        # Implement the logic to calculate possible moves for a rook
-        pass
+        # Move horizontally
+        possible_moves = []
+        for i in range(self.x + 1, 8):
+            if self.board[self.y][i] is None:
+                possible_moves.append(Position(i, self.y))
+            else:
+                if self.board[self.y][i].color != self.color:
+                    possible_moves.append(Position(i, self.y))
+                break
+        for i in range(self.x - 1, -1, -1):
+            if self.board[self.y][i] is None:
+                possible_moves.append(Position(i, self.y))
+            else:
+                if self.board[self.y][i].color != self.color:
+                    possible_moves.append(Position(i, self.y))
+                break
+        # Move vertically
+        for i in range(self.y + 1, 8):
+            if self.board[i][self.x] is None:
+                possible_moves.append(Position(self.x, i))
+            else:
+                if self.board[i][self.x].color != self.color:
+                    possible_moves.append(Position(self.x, i))
+                break
+        for i in range(self.y - 1, -1, -1):
+            if self.board[i][self.x] is None:
+                possible_moves.append(Position(self.x, i))
+            else:
+                if self.board[i][self.x].color != self.color:
+                    possible_moves.append(Position(self.x, i))
+                break
 
 class Knight(ChessPiece):
     def __str__(self) -> str:
